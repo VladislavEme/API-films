44084bfb9b6665766a9e97418fe0f8a027137345
Vlad Em <haos1996haos@gmail.com>
2023-01-08 16:19:20 +0300

    added input search

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import searchReducer from './searchSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    search: searchReducer,
   },
 });
 
